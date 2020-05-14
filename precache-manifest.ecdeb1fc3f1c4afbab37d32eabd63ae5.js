430ab8bdea1a32a28b0791835070998b882ea7c7
Ashwani Gupta <Ashhwanigupta1@gmail.com>
2020-05-14 12:18:21 +0530

    Updates

---
+++ precache-manifest.ecdeb1fc3f1c4afbab37d32eabd63ae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5d2b7e92d8690999d193f9e01c4779",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d5432f7764f8254ad7b1",
+    "url": "/react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0d6f57c96f6d92267f42",
+    "url": "/react/static/css/main.e0c60e4a.chunk.css"
+  },
+  {
+    "revision": "d5432f7764f8254ad7b1",
+    "url": "/react/static/js/2.c0e171f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react/static/js/2.c0e171f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6f57c96f6d92267f42",
+    "url": "/react/static/js/main.d0c1aff6.chunk.js"
+  },
+  {
+    "revision": "84f2863c9e71e74d46c1",
+    "url": "/react/static/js/runtime-main.505757bb.js"
+  }
+]);
